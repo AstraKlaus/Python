b9291fd9be295ae3c347cdffc0e8841448bb43e7
AstraKlaus <Endrew-K@yandex.ru>
2021-12-20 13:52:15 +0400

    complete first task

--- L7(2) Kasyanov IT-112.py
+++ L7(2) Kasyanov IT-112.py
@@ -1,9 +1,11 @@
 import random
 
-n = int(input("How many cars?"))
+n = int(input("How many cars? "))
 speed = [round(random.uniform(20, 150), 2) for i in range(n)]
 time = [round(random.uniform(0, 24), 2) for j in range(n)]
-way = [k * p for k in speed for p in time]
+way = [round(speed[k] * time[k],2) for k in range(len(speed))]
+
+print(f"Данные о скорости: {speed} \nДанные о времени: {time} \nДанные о пути: {way}")
 
 mid_speed = 0
 for i in speed:
@@ -19,3 +21,21 @@ mid_way = 0
 for i in speed:
     mid_way += i
 mid_way = round(mid_way / len(way), 2)
+
+lowest = []
+middle = []
+highest = []
+
+for i in range(len(speed)):
+    if speed[i] < mid_speed and time[i] < mid_time and way[i] < mid_way:
+        lowest.append(i+1)
+    elif speed[i] > mid_speed and time[i] > mid_time and way[i] > mid_way:
+        middle.append(i+1)
+    else: highest.append(i+1)
+
+print(f"Средняя скорость: {mid_speed}\nСреднее время: {mid_time}\nСреднее расстояние: {mid_way}\n")
+
+print(f"Первая группа (номера машин): {lowest}\nВторая группа (номера машин): {middle}\nТретья группа (номера машин): {highest}")
+
+
+
