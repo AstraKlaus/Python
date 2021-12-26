8c5a2fe952bb5bfd649f78f53b58e75b0fc3add4
AstraKlaus <Endrew-K@yandex.ru>
2021-12-26 14:33:23 +0400

    complete

--- L6(2) Kasyanov IT-112.py
+++ L6(2) Kasyanov IT-112.py
@@ -1,147 +1,154 @@
 import random
-
-x = int(input(f"Введите число: "))
-while x > 0:
-    print(x%10, end='')
-    x//=10
-
-x = int(input(f"Введите число отличное от 0: "))
-while x != 1:
-    if x % 2 == 0:
-        x //= 2
-    elif x % 2 != 0:
-        x = (x * 3 + 1) // 2
-print(x)
-
-count = int(input("Введите количество чисел: "))
-k = 0
-for i in range(1, count + 1):
-    number = int(input("Введите простое число: "))
-    for i in range(2, number // 2 + 1):
-        if number % i == 0:
-            k += 1
-            break
-print(f"Количество простых чисел {k}")
-
-
-x = int(input(f"Введите число: "))
-ls=0
-while x > 0:
-    if x%10 == 8:
-        ls = k
-    x //=10
-print(f"Номер цифры 8: {ls}")
-
-a_1 = 0
-b_2 = 0
-while True:
-    a = int(input("команда 1: "))
-    b = int(input("команда 2: "))
-    if a == 0 and b == 0:
-        break
-    a_1 += a
-    b_2 += b
-    print("1 команда - ", a_1, "/", b_2, " - команда 2")
-
-if a_1 > b_2:
-    print("победила команда номер 1 со счетом", a_1, "/", b_2)
-elif a_1 == b_2:
-    print("ничья со счетом", b_2, "/", a_1)
-else:
-    print("победила команда номер 2 со счетом", b_2, "/", a_1)
-
-v1, v2, v3, v4, v5, v6, v7, = 0,0,0,0,0,0,0
-
-n = random.randint(1,200)
-
-while n != 0:
-    while n - 64 > 0:
-        n -= 64
-        v7 += 1
-    while n - 32 > 0:
-        n -= 32
-        v6 += 1
-    while n - 16 > 0:
-        n -= 16
-        v5 += 1
-    while n - 8 > 0:
-        n -= 8
-        v4 += 1
-    while n - 4 > 0:
-        n -= 4
-        v3 += 1
-    while n - 2 > 0:
-        n -= 2
-        v2 += 1
-    while n - 1 > 0:
-        n -= 1
-        v1 += 1
-print(f"Использованные купюры:\nКупюры по 1: {v1}\nКупюры по 2: {v2}\nКупюры по 4: {v3}\nКупюры по 8: {v4}\nКупюры по 16: {v5}\nКупюры по 32: {v6}\nКупюры по 64: {v7}\nОбщее количество куплюр: {v1+v2+v3+v4+v5+v6+v7}")
-
-x = random.randint(100,100000)
-summary = 0
-m = int(input("Введите сколько надо суммировать последних цифр"))
-while m>0:
-    summary += x%10
-    x //= 10
-    m-=1
-print(f"Сумма равна {summary}")
-
-n = int(input("Введите число"))
-i = 2
-while i <= n:
-    i = i + 1
-    if n % i == 0:
-        print(f"Минимальный делитель: {i}")
-        break
-
-x = int(input("Введите число х: "))
-a = int(input("Введите число а: "))
-k = int(input("Введите число k: "))
-b = int(input("Введите число b: "))
-o = j = 0
-while x > 0:
-    if x%10 == a:
-        print("1) Да")
-        j += 1
-    if j > k:
-        print("2) Да")
-    if x%10 == b:
-        o += 1
-    if j > 0 and o > 0:
-        print("3) Да")
-
-x = random.randint(1,30)
-y = random.randint(1,30)
-z = random.randint(1,30)
-
-k = 0
-while x > 0:
-    while y > 0:
-        while z > 0:
-            if x**2 + y ** 2 == z ** 2 or z**2 + y ** 2 == x ** 2 or x**2 + z ** 2 == y ** 2:
+question = int(input("Какой номер проверить? ")):
+if question==1:
+    x = int(input(f"Введите число: "))
+    while x > 0:
+        print(x%10, end='')
+        x//=10
+if question == 3:
+    x = int(input(f"Введите число отличное от 0: "))
+    while x != 1:
+        if x % 2 == 0:
+            x //= 2
+        elif x % 2 != 0:
+            x = (x * 3 + 1) // 2
+    print(x)
+if question == 4:
+    count = int(input("Введите количество чисел: "))
+    k = 0
+    for i in range(1, count + 1):
+        number = int(input("Введите простое число: "))
+        for i in range(2, number // 2 + 1):
+            if number % i == 0:
                 k += 1
-            z -= 1
-        y -= 1
-    x -= 1
-print(f"Всего {k} пифагоровы тройки")
+                break
+    print(f"Количество простых чисел {k}")
+if question == 5:
 
+    x = int(input(f"Введите число: "))
+    ls=0
+    while x > 0:
+        if x%10 == 8:
+            ls = k
+        x //=10
+    print(f"Номер цифры 8: {ls}")
+if question == 6:
+    a_1 = 0
+    b_2 = 0
+    while True:
+        a = int(input("команда 1: "))
+        b = int(input("команда 2: "))
+        if a == 0 and b == 0:
+            break
+        a_1 += a
+        b_2 += b
+        print("1 команда - ", a_1, "/", b_2, " - команда 2")
+    if a_1 > b_2:
+        print("победила команда номер 1 со счетом", a_1, "/", b_2)
+    elif a_1 == b_2:
+        print("ничья со счетом", b_2, "/", a_1)
+    else:
+        print("победила команда номер 2 со счетом", b_2, "/", a_1)
+if question == 7:
 
-n = int(input("Введите число: "))
+    v1, v2, v3, v4, v5, v6, v7, = 0,0,0,0,0,0,0
 
-i=0
-j=0
-k=0
-while i != 9:
-    while j != 9:
-        while k != 9:
-            if i+j+k==n:
-                print(i*100+j*10+k)
-            i +=1
-            j +=1
-            k +=1
-        k=0
-    j = 0
+    n = random.randint(1,200)
+
+    while n != 0:
+        while n - 64 > 0:
+            n -= 64
+            v7 += 1
+        while n - 32 > 0:
+            n -= 32
+            v6 += 1
+        while n - 16 > 0:
+            n -= 16
+            v5 += 1
+        while n - 8 > 0:
+            n -= 8
+            v4 += 1
+        while n - 4 > 0:
+            n -= 4
+            v3 += 1
+        while n - 2 > 0:
+            n -= 2
+            v2 += 1
+        while n - 1 > 0:
+            n -= 1
+            v1 += 1
+    print(f"Использованные купюры:\nКупюры по 1: {v1}\nКупюры по 2: {v2}\nКупюры по 4: {v3}\nКупюры по 8: {v4}\nКупюры по 16: {v5}\nКупюры по 32: {v6}\nКупюры по 64: {v7}\nОбщее количество куплюр: {v1+v2+v3+v4+v5+v6+v7}")
+if question == 8:
+    x = random.randint(100,100000)
+    summary = 0
+    m = int(input("Введите сколько надо суммировать последних цифр"))
+    while m>0:
+        summary += x%10
+        x //= 10
+        m-=1
+    print(f"Сумма равна {summary}")
+if question == 9:
+    n = int(input("Введите число"))
+    i = 2
+    while i <= n:
+        i = i + 1
+        if n % i == 0:
+            print(f"Минимальный делитель: {i}")
+            break
+if question == 11:
+    x = int(input("Введите число х: "))
+    a = int(input("Введите число а: "))
+    k = int(input("Введите число k: "))
+    b = int(input("Введите число b: "))
+    o = j = 0
+    while x > 0:
+        if x%10 == a:
+            print("1) Да")
+            j += 1
+        if j > k:
+            print("2) Да")
+        if x%10 == b:
+            o += 1
+        if j > 0 and o > 0:
+            print("3) Да")
+if question == 12:
+    x = random.randint(1,30)
+    y = random.randint(1,30)
+    z = random.randint(1,30)
+
+    k = 0
+    while x > 0:
+        while y > 0:
+            while z > 0:
+                if x**2 + y ** 2 == z ** 2 or z**2 + y ** 2 == x ** 2 or x**2 + z ** 2 == y ** 2:
+                    k += 1
+                z -= 1
+            y -= 1
+        x -= 1
+    print(f"Всего {k} пифагоровы тройки")
+
+if question == 13:
+    m = int(input("Введите m "))
+    n = int(input("Введите n "))
+    summary = 0
+    for i in range(1, m):
+        summary += i**n
+if question == 14:
+    n = int(input("Введите число: "))
+
+    i=0
+    j=0
+    k=0
+    while i != 9:
+        while j != 9:
+            while k != 9:
+                if i+j+k==n:
+                    print(i*100+j*10+k)
+                i +=1
+                j +=1
+                k +=1
+            k=0
+        j = 0
 
 
 
--- L7(2) Kasyanov IT-112.py
+++ L7(2) Kasyanov IT-112.py
@@ -1,9 +1,17 @@
 import random
 
+def spacing(sal):
+    general = int(round(sal // 1, 0))
+    extra = int(round((sal % 1) * 100, 0))
+    if general < 1000:
+        return '{} руб. {} коп.'.format(str(general), extra)
+    else:
+        return '{} {} руб. {} коп.'.format(general // 1000, str(general)[-3::], extra)
+
 n = int(input("How many cars? "))
 speed = [round(random.uniform(20, 150), 2) for i in range(n)]
 time = [round(random.uniform(0, 24), 2) for j in range(n)]
-way = [round(speed[k] * time[k],2) for k in range(len(speed))]
+way = [round(speed[k] * time[k], 2) for k in range(len(speed))]
 
 print(f"Данные о скорости: {speed} \nДанные о времени: {time} \nДанные о пути: {way}")
 
@@ -13,12 +21,12 @@ for i in speed:
 mid_speed = round(mid_speed / len(speed), 2)
 
 mid_time = 0
-for i in speed:
+for i in time:
     mid_time += i
 mid_time = round(mid_time / len(time), 2)
 
 mid_way = 0
-for i in speed:
+for i in way:
     mid_way += i
 mid_way = round(mid_way / len(way), 2)
 
@@ -28,14 +36,48 @@ highest = []
 
 for i in range(len(speed)):
     if speed[i] < mid_speed and time[i] < mid_time and way[i] < mid_way:
-        lowest.append(i+1)
+        lowest.append(i + 1)
     elif speed[i] > mid_speed and time[i] > mid_time and way[i] > mid_way:
-        middle.append(i+1)
-    else: highest.append(i+1)
+        middle.append(i + 1)
+    else:
+        highest.append(i + 1)
 
 print(f"Средняя скорость: {mid_speed}\nСреднее время: {mid_time}\nСреднее расстояние: {mid_way}\n")
 
-print(f"Первая группа (номера машин): {lowest}\nВторая группа (номера машин): {middle}\nТретья группа (номера машин): {highest}")
+print(f"Первая группа (номера машин): {lowest}\nВторая группа (номера машин): {middle}\nТретья группа (номера машин): {highest}\n")
 
 
+orders = [random.randint(1, 100) for i in range(72*3+1)]
+positions = [f"Продукт №{i}" for i in range(1, 72*3+2)]
+costs = [round(random.uniform(1, 100), 2) for i in range(72*3+1)]
 
+print(f"Дано: \nЗаказы: {orders}\nТовар: {positions}\nЦены: {costs}\n\nВывод программы:\nСмета")
+
+tax = 0
+summary_1 = 0
+summary_2 = 0
+summary_3 = 0
+for number in range(0, len(orders)-1, 3):
+    company = 1 if number < 72 else 2 if number < 144 else 3
+    tax = (tax) % 24 + 1
+    print(f"{company} Компания\nЗазказ {tax}\n{positions[number]} - {orders[number]} шт. по {costs[number]} = {spacing(orders[number]*costs[number])}\n"
+          f"{positions[number+1]} - {orders[number+1]} шт. по {costs[number+1]} = {spacing(orders[number+1]*costs[number+1])}\n"
+          f"{positions[number+2]} - {orders[number+2]} шт. по {costs[number+2]} = {spacing(orders[number+2]*costs[number+2])}\n"
+          f"Всего с {tax} заказа {spacing(orders[number]*costs[number]+orders[number+1]*costs[number+1]+orders[number+2]*costs[number+2])}")
+    if number < 73:
+        for i in range(len(orders)):
+            summary_1 += orders[number]*costs[number]+orders[number+1]*costs[number+1]+orders[number+2]*costs[number+2]
+    if number == 69:
+        print(f"Всего с компании I {spacing(summary_1)}")
+    if 145 > number > 73:
+        for i in range(len(orders)):
+            summary_2 += orders[number]*costs[number]+orders[number+1]*costs[number+1]+orders[number+2]*costs[number+2]
+    if number == 141:
+        print(f"Всего с компании II {spacing(summary_2)}")
+    if number > 145:
+        for i in range(len(orders)):
+            summary_3 += orders[number]*costs[number]+orders[number+1]*costs[number+1]+orders[number+2]*costs[number+2]
+    if number >210:
+        print(f"Всего с компании III {spacing(summary_3)}")
+
+print(f"\nВсего со всех компаний {spacing(summary_1 + summary_2 + summary_3)}")
